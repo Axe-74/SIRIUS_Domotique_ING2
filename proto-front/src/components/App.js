dd0f82fa65e8a8e24cf2a84ef013395acd439dce
Enzo Dupuis <enzo.dupuis@etu.u-pec.fr>
2025-12-01 14:51:22 +0100

    Ajout Helloworld sur le front et correction de bug

--- proto-front/src/components/App.js
+++ proto-front/src/components/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import logo from '../assets/logo.jpeg';
 import '../styles/App.css';
 
+
 export default function App() {
   return (
     <div className="App">
-
-        <img src={logo} alt="logo" />
+        <img src={logo} alt="logo" className="logo" />
+        <textarea
+            value="Hello world"
+            readOnly
+            className="readonly-text"
+        />
     </div>
   );
 }
