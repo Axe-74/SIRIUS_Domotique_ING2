421381ef3e2a2171a81e93b27e2e8d98342e75dd
Tom <tom.da-rocha@etu.u-pec.fr>
2026-01-21 23:40:06 +0100

    Reglage des conflits avec la derniere version de master avant la pull request

--- front/src/setupProxy.js
+++ front/src/setupProxy.js
@@ -3,6 +3,6 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(
         '/api',
-        createProxyMiddleware({ target: 'http://localhost:8080/api' })
+        createProxyMiddleware({ target: 'http://localhost:8080' })
     );
 };
