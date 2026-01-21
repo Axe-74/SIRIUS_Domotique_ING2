7b355aaa1af6659515f4cc41580767015f8deffe
Axel <axel.jacomme@etu.u-pec.fr>
2026-01-22 00:15:41 +0100

    Update de la page d'accueil

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import logo from '../assets/logo.jpeg';
 import '../styles/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import smartHome from '../assets/smart-home.jpg';
 
 
 export default function App() {
   return (
     <div className="App">
         <img src={logo} alt="logo" className="logo" />
+        <img src={smartHome} alt="smarthome" className="smarthome" />
         <textarea
-            value="Hello world"
+            value="Bienvenue dans votre application de domotique !"
             readOnly
             className="readonly-text"
         />
