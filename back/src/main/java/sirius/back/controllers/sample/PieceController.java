b97f35392fc0f323cf8fa690daab105f1e0fd7d1
TDR917 <tomdarocha917@gmail.com>
2026-01-21 23:52:26 +0100

    Update PieceController.java

--- back/src/main/java/sirius/back/controllers/sample/PieceController.java
+++ back/src/main/java/sirius/back/controllers/sample/PieceController.java
@@ -18,37 +18,10 @@ public class PieceController {
     @Autowired
     private PieceService pieceService;
 
-//    @GetMapping("/{id}")
-//    public ResponseEntity<Sample> findById(@PathVariable Long id){
-//
-//        return new ResponseEntity<>(sampleService.findByIdSample(id), HttpStatus.OK);
-//    }
 
     @GetMapping("all")
     public ResponseEntity<List<Piece>> findAllPiece(){
         return new ResponseEntity<>(pieceService.findAllPiece(), HttpStatus.OK);
     }
 
-//    @GetMapping("dateSample")
-//    public ResponseEntity<Sample> findLastSampleByDate() {
-//        return new ResponseEntity<>(sampleService.findOldestSample(), HttpStatus.OK);
-//    }
-//
-//    @PostMapping("update")
-//    public ResponseEntity<Sample> updateSample(@RequestBody Sample sample){
-//        boolean isUpdated = sampleService.updateSampleDate(sample);
-//        if(!isUpdated){
-//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//        }
-//        return new ResponseEntity<>(sample, HttpStatus.OK);
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<Long> deleteMapping(@PathVariable Long id){
-//        boolean isRemoved = sampleService.deleteSample(id);
-//        if(!isRemoved){
-//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//        }
-//        return  new ResponseEntity<>(id, HttpStatus.OK);
-//    }
 }
