63812b66fc37d177514ec98237b52dc9f50e3bb6
TDR917 <tomdarocha917@gmail.com>
2026-01-21 23:54:05 +0100

    Update PieceService.java

--- back/src/main/java/sirius/back/services/sample/PieceService.java
+++ back/src/main/java/sirius/back/services/sample/PieceService.java
@@ -16,36 +16,9 @@ public class PieceService {
     @Autowired
     private PieceRepository pieceRepository;
 
-//    public Sample findOldestSample(){
-//        return sampleRepository.findLastSampleByDate();
-//    }
-//
-//    public Sample findByIdSample(Long idSample) {
-//        Optional<Sample> optionalSample = sampleRepository.findById(idSample);
-//        return optionalSample.orElse(null);
-//    }
 
     public List<Piece> findAllPiece(){
         return pieceRepository.findAll();
     }
 
-//    public boolean updateSampleDate(Sample updatedSample) {
-//        Optional<Sample> optionalSample = sampleRepository.findById(updatedSample.getIdSample());
-//        if(optionalSample.isPresent()){
-//            Sample sample = optionalSample.get();
-//            sample.setDateSample(updatedSample.getDateSample());
-//            sampleRepository.saveAndFlush(sample);
-//            return true;
-//        }
-//        return false;
-//    }
-//
-//    public boolean deleteSample(Long idSample) {
-//        Optional<Sample> optionalSample = sampleRepository.findById(idSample);
-//        if (optionalSample.isPresent()) {
-//            optionalSample.ifPresent(sample -> sampleRepository.delete(sample));
-//            return true;
-//        }
-//        return false;
-//    }
 }
