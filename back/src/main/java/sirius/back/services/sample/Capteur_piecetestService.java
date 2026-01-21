e2bd3d687caeaf83fa453b7bcf297b56e94a450a
TDR917 <tomdarocha917@gmail.com>
2026-01-21 23:53:42 +0100

    Update Capteur_piecetestService.java

--- back/src/main/java/sirius/back/services/sample/Capteur_piecetestService.java
+++ back/src/main/java/sirius/back/services/sample/Capteur_piecetestService.java
@@ -13,36 +13,9 @@ public class Capteur_piecetestService {
     @Autowired
     private Capteur_testpieceRepository capteurTestpieceRepository;
 
-//    public Sample findOldestSample(){
-//        return sampleRepository.findLastSampleByDate();
-//    }
-//
-//    public Sample findByIdSample(Long idSample) {
-//        Optional<Sample> optionalSample = sampleRepository.findById(idSample);
-//        return optionalSample.orElse(null);
-//    }
 
     public List<Capteur_testpiece> findAllCapteur_testpiece() {
         return capteurTestpieceRepository.findAll();
     }
 
-//    public boolean updateSampleDate(Sample updatedSample) {
-//        Optional<Sample> optionalSample = sampleRepository.findById(updatedSample.getIdSample());
-//        if(optionalSample.isPresent()){
-//            Sample sample = optionalSample.get();
-//            sample.setDateSample(updatedSample.getDateSample());
-//            sampleRepository.saveAndFlush(sample);
-//            return true;
-//        }
-//        return false;
-//    }
-//
-//    public boolean deleteSample(Long idSample) {
-//        Optional<Sample> optionalSample = sampleRepository.findById(idSample);
-//        if (optionalSample.isPresent()) {
-//            optionalSample.ifPresent(sample -> sampleRepository.delete(sample));
-//            return true;
-//        }
-//        return false;
-//    }
 }
