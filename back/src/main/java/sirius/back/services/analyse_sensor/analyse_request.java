23d7fd3adc036ce3fb313b5e35eab260c23f8088
Enzo Dupuis <enzo.dupuis@etu.u-pec.fr>
2026-01-22 15:12:20 +0100

    Correction bug suite à la suppréssion d'une mauvaise classe

--- back/src/main/java/sirius/back/services/analyse_sensor/analyse_request.java
+++ back/src/main/java/sirius/back/services/analyse_sensor/analyse_request.java
@@ -1,4 +1,4 @@
-package sirius.back.services.analyse_sensor; // Vérifie que le package est bon
+package sirius.back.services.analyse_sensor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -13,20 +13,15 @@ public class analyse_request implements CommandLineRunner {
 
     @Autowired
     private AutomatisationService automatisationService;
-    @Autowired
-    private mesure_v1Service mesure_v1Service;
 
     @Scheduled(fixedRate = 100, initialDelay = 0)
     public void run(String... args) throws Exception {
-    //    System.out.println("Test SQL");
-
 
         try{
             automatisationService.verifierEtMettreAJourAutomatisation();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
-        //System.out.println("Test SQL");
+        System.out.println("Test SQL");
     }
 }
