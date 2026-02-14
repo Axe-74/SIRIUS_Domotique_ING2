bf93a6175381f2152597af30080fe8766840b9bf
Axel <axel.jacomme@etu.u-pec.fr>
2026-02-14 22:19:23 +0100

    Ajout de la requête pour sélectionner un certain nombre de mesures uniquement

--- back/src/main/java/sirius/back/repositories/mesure_v1Repository.java
+++ back/src/main/java/sirius/back/repositories/mesure_v1Repository.java
@@ -2,6 +2,7 @@ package sirius.back.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import sirius.back.models.mesure_v1;
 
@@ -12,6 +13,9 @@ public interface mesure_v1Repository extends JpaRepository<mesure_v1, Long> {
     @Query(value = "SELECT DISTINCT ON (id_capteur) * FROM mesure_v1 ORDER BY id_capteur, id_mesure DESC;",nativeQuery = true)
     List<mesure_v1> findLatestMesure();
 
-@Query(value = "SELECT * FROM mesure_v1 AS a ORDER BY a.id_mesure DESC LIMIT 1",nativeQuery = true)
-mesure_v1 findOldestMesure();
+    @Query(value = "SELECT * FROM mesure_v1 AS a ORDER BY a.id_mesure DESC LIMIT 1",nativeQuery = true)
+    mesure_v1 findOldestMesure();
+
+    @Query(value = "SELECT * FROM mesure_v1 WHERE id_capteur = :idCapteur ORDER BY date DESC LIMIT :nombreMesures", nativeQuery = true)
+    List<mesure_v1> recupererHistorique(@Param("idCapteur") Integer idCapteur, @Param("nombreMesures") Integer nombreMesures);
 }
