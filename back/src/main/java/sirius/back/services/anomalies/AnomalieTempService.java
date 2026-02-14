9c8db801911c195f9ec4eb8672ab636db8a82253
Axel <axel.jacomme@etu.u-pec.fr>
2026-02-14 22:17:11 +0100

    Ajout de la V2 de la détection d'anomalies, et modification de la V1 pour que les seuils soient récupérés en base.

--- back/src/main/java/sirius/back/services/anomalies/AnomalieTempService.java
+++ back/src/main/java/sirius/back/services/anomalies/AnomalieTempService.java
@@ -1,28 +1,58 @@
 package sirius.back.services.anomalies;
 
 import org.springframework.stereotype.Service;
+import sirius.back.models.ConfigTempAnomalies;
 import sirius.back.models.mesure_v1;
+import java.util.List;
 
 @Service
 public class AnomalieTempService {
-    public static double seuilMin =  -5.0;
-    public static double seuilMax = 40.0;
 
-    public static boolean verifierSeuils(mesure_v1 mesure){
+    public static boolean verifierSeuils(mesure_v1 mesure, ConfigTempAnomalies config){
         double valeur = mesure.getValeur();
+        if (config == null) return true;
 
-        if(valeur < seuilMin){
+        if(valeur < config.getSeuilMinAbsolu()){
             System.err.println("Anomalie détectée. Température anormalement basse :" + valeur + "°C.");
             return false ;
         }
 
-        if(valeur > seuilMax){
+        if(valeur > config.getSeuilMaxAbsolu()){
             System.err.println("Anomalie détectée. Température anormalement haute :" + valeur + "°C.");
             return false;
         }
 
         else {
             System.out.println("Valeur dans l'intervalle accepté.");
+            return true;
+        }
+    }
+
+    public boolean verifierCoherence(mesure_v1 nouvelleMesure, ConfigTempAnomalies config, List<mesure_v1> historique) {
+
+        if (historique == null || historique.isEmpty()) {
+            return true;
+        }
+
+        // moyenne glissante
+        double somme = 0;
+        for (mesure_v1 m : historique) {
+            somme += m.getValeur();
+        }
+        double moyenne = somme / historique.size();
+
+        //ecart
+        double nouvelleValeur = nouvelleMesure.getValeur();
+        double ecart = Math.abs(nouvelleValeur - moyenne);
+
+        //validation
+        if (ecart > config.getSeuilEcartMoyenne()) {
+            System.err.println("Incohérence, écart avec les précédentes valeurs supérieur à la valeur maximum autorisée de " + config.getSeuilEcartMoyenne() + ".");
+            return false;
+        }
+
+        else {
+            System.out.println("Valeur cohérente avec les précédentes.");
             return true;
         }
     }
