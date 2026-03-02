43fca8c4bed9c4a6b86f71700b8ceb402f10a11f
Enzo Dupuis <enzo.dupuis@etu.u-pec.fr>
2026-03-02 10:02:04 +0100

    Ajout classe pour faire les requêtes dans la nouvelle classe Parametre_objet
    Modification du modèle Automatisation pour faire la jointure

---
+++ back/src/main/java/sirius/back/repositories/Parametre_objetRepository.java
@@ -0,0 +1,7 @@
+package sirius.back.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sirius.back.models.Parametre_objet;
+
+public interface Parametre_objetRepository extends JpaRepository<sirius.back.models.Parametre_objet, Long> {}
+
