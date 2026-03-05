74a4a32aa2b63f4dd3ee53ab859d0bcc43b5f82d
Tom <tom.da-rocha@etu.u-pec.fr>
2026-03-05 21:48:56 +0100

    panneau ne s'affiche qu'au clique d'une piece

--- front/src/components/Maison.js
+++ front/src/components/Maison.js
@@ -149,35 +149,38 @@ export default function Maison() {
 
     if (infoPiece !== undefined) {
         dansInfoPiece = (
-            <div>
-                <p> Etage : {infoPiece.etage} </p>
-                <p> Pièce : {infoPiece.nom} </p>
-                <p> Dimensions : {infoPiece.width / RATIO}m x {infoPiece.height / RATIO}m </p>
-                <h5> Capteur(s) </h5>
-                <ul>
-                    {capteurs_testpiece
-                        .filter(function (c) {
-                            return c.id_piece === infoPiece.id_piece;
-                        })
-                        .map(function (c) {
+            <div className="conteneur-info-piece">
+                <h4> Infos pièces </h4>
+                <div>
+                    <p> Etage : {infoPiece.etage} </p>
+                    <p> Pièce : {infoPiece.nom} </p>
+                    <p> Dimensions : {infoPiece.width / RATIO}m x {infoPiece.height / RATIO}m </p>
+                    <h5> Capteur(s) </h5>
+                    <ul>
+                        {capteurs_testpiece
+                            .filter(function (c) {
+                                return c.id_piece === infoPiece.id_piece;
+                            })
+                            .map(function (c) {
+                                return (
+                                    <li key={c.id_capteur_testpiece}>
+                                        {c.nom} : {c.etat}
+                                    </li>
+                                );
+                            })
+                        }
+                    </ul>
+                    <h5> Objet(s) </h5>
+                    <ul>
+                        {infoPiece.objets.map(function (o) {
                             return (
-                                <li key={c.id_capteur_testpiece}>
-                                    {c.nom} : {c.etat}
+                                <li key={o.id_objet}>
+                                    {o.nom_objet} : {o.etat ? 'ON' : 'OFF'}
                                 </li>
                             );
-                        })
-                    }
-                </ul>
-                <h5> Objet(s) </h5>
-                <ul>
-                    {infoPiece.objets.map(function (o) {
-                        return (
-                            <li key={o.id_objet}>
-                                {o.nom_objet} : {o.etat ? 'ON' : 'OFF'}
-                            </li>
-                        );
-                    })}
-                </ul>
+                        })}
+                    </ul>
+                </div>
             </div>
         );
     } else {
@@ -295,8 +298,7 @@ export default function Maison() {
                     </Stage>
                 </div>
 
-                <div className="conteneur-info-piece">
-                    <h4> Infos pièces </h4>
+                <div>
                     {dansInfoPiece}
                 </div>
 
