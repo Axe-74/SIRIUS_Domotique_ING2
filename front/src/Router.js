ef644124891efc4fbe35a693ca88f10fba4a125e
Axel <axel.jacomme@etu.u-pec.fr>
2026-03-06 18:11:40 +0100

    Déplacement de Navbar.js et Router.js au même endroit que dans le dernier proto + adaptation de index.js à ce changement

--- front/src/Router.js
+++ front/src/Router.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import App from "./App";
+import App from "./components/App";
 import Navbar from "./Navbar";
-import NotFound from "./NotFound";
-import MaisonView from "../modules/maison/views/MaisonView";
-import AutomatisationPage from "../modules/Automatisation/views/AutomatisationPage";
-import ScenarioPage from "../modules/Scenario/views/ScenarioPage";
-import SimulationViews from "../modules/simulation/views/simulationViews";
-import MesuresHistoriqueViews from "../modules/mesures/views/mesuresHistoriqueViews";
-import MesuresActuelleViews from "../modules/mesures/views/mesuresActuelleViews";
+import NotFound from "./components/NotFound";
+import MaisonView from "./modules/maison/views/MaisonView";
+import AutomatisationPage from "./modules/Automatisation/views/AutomatisationPage";
+import ScenarioPage from "./modules/Scenario/views/ScenarioPage";
+import SimulationViews from "./modules/simulation/views/simulationViews";
+import MesuresHistoriqueViews from "./modules/mesures/views/mesuresHistoriqueViews";
+import MesuresActuelleViews from "./modules/mesures/views/mesuresActuelleViews";
 
 
 export default function Router () {
