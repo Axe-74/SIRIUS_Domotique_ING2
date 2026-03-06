df224054ca4e137a2b2a3232944095364e7242ba
Axel <axel.jacomme@etu.u-pec.fr>
2026-03-06 17:46:41 +0100

    Adaptation du Router avec les nouveaux chemins maintenant que les fichiers ont été réécrits

--- front/src/components/Router.js
+++ front/src/components/Router.js
@@ -3,12 +3,12 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import App from "./App";
 import Navbar from "./Navbar";
 import NotFound from "./NotFound";
-import MesureTempActuelle from "./MesureTempActuelle";
 import Automatisation from "./Automatisation";
 import Maison from "./Maison";
 import Scenario from "./Scenario";
-import SimulationJournee from "./SimulationJournee";
-import Historique from "./Historique";
+import SimulationViews from "../modules/simulation/views/simulationViews";
+import MesuresHistoriqueViews from "../modules/mesures/views/mesuresHistoriqueViews";
+import MesuresActuelleViews from "../modules/mesures/views/mesuresActuelleViews";
 
 
 export default function Router () {
@@ -18,11 +18,11 @@ export default function Router () {
                 <Navbar />
                 <Routes>
                     <Route path="/" element={<App />}/>
-                    <Route path="/mesures_temp" element={<MesureTempActuelle />}/>
                     <Route path="/automatisation" element={<Automatisation />}/>
-                    <Route path="/simulation_journee" element={<SimulationJournee />}/>
+                    <Route path="/simulation_journee" element={<SimulationViews />}/>
                     <Route path="/maison" element={<Maison />} />
-                    <Route path="/historique" element={<Historique />} />
+                    <Route path="/historique" element={<MesuresHistoriqueViews />} />
+                    <Route path="/mesures_temp" element={<MesuresActuelleViews />}/>
                     <Route path="/scenario" element={<Scenario />} />
                     <Route path="*" element={<NotFound />}/>
                 </Routes>
