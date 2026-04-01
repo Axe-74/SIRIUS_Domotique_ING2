afa569f9b256232332f471829f64a67d38e7b0b8
Enzo Dupuis <enzo.dupuis@etu.u-pec.fr>
2026-04-01 09:35:12 +0200

    Création d'une méthode pour update les objets en fonction de la simulation d'une journée

--- back/src/main/java/sirius/back/services/Parametre_objetService.java
+++ back/src/main/java/sirius/back/services/Parametre_objetService.java
@@ -6,6 +6,7 @@ import org.springframework.transaction.annotation.Transactional;
 import sirius.back.models.Parametre_objet;
 import sirius.back.repositories.Parametre_objetRepository;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,21 +15,74 @@ import java.util.Map;
 public class Parametre_objetService {
 
     @Autowired
-    private Parametre_objetRepository parametreObjetRepository;
+    private Parametre_objetRepository Parametre_objetRepository;
 
     @Transactional
-    public void mettreAJourSpecification() {
-        List<Parametre_objet> parametreObjets = parametreObjetRepository.findAll();
-        for (Parametre_objet parametreObjet : parametreObjets) {
-            Map<String, Object> specs = parametreObjet.getDonneesJson();
-            if (specs == null) {
-                specs = new HashMap<>();
-            }
-            specs.put("puissance", 1600);
-            specs.put("mode", "Eco");
+    public void agirSurObjets(double temperature, LocalDateTime heure) {
+        List<Parametre_objet> objets = Parametre_objetRepository.findAll();
 
-            parametreObjet.setDonneesJson(specs);
-            parametreObjetRepository.save(parametreObjet);
+        boolean Nuit = (heure.getHour() < 7 || heure.getHour() > 22);
+        boolean Soir = (heure.getHour() >= 18 && heure.getHour() <= 23);
+        boolean Jour = (heure.getHour() >= 7 && heure.getHour() < 18);
+
+
+        for (Parametre_objet objet : objets) {
+            Map<String, Object> specs = objet.getDonneesJson();
+            if (specs == null) specs = new HashMap<>();
+
+            String nom = objet.getNom_objet().toLowerCase();
+
+            if (nom.contains("fenêtre")) {
+                if (temperature > 22.0 && temperature < 26.0 && !Nuit) {
+                    specs.put("degre_ouverture", 30);
+                } else {
+                    specs.put("degre_ouverture", 0);
+                }
+            }
+            else if (nom.contains("volet")) {
+                if (Nuit) {
+                    specs.put("niveau_ouverture", 0);
+                } else if (temperature > 27.0) {
+                    specs.put("niveau_ouverture", 20);
+                } else {
+                    specs.put("niveau_ouverture", 100);
+                }
+            }
+            else if (nom.contains("radiateur")) {
+                if (temperature < 19.0) {
+                    objet.setetat(true);
+                    specs.put("puissance", 1500);
+                    specs.put("temperature", 21.0);
+                } else {
+                    objet.setetat(false);
+                    specs.put("puissance", 0);
+                    specs.put("temperature", 18.0);
+                }
+            }
+            else if (nom.contains("Climatisation")) {
+                if (temperature > 25.0) {
+                    objet.setetat(true);
+                    specs.put("temperature", 22.0);
+                } else {
+                    objet.setetat(false);
+                }
+            }
+            else if (nom.contains("lumiere")) {
+                boolean periode = Soir || Nuit;
+                objet.setetat(periode);
+                specs.put("allumee", periode);
+                if (periode = Soir) {
+                    specs.put("intensite", 100);                    
+                }
+                else if (periode = Nuit) {
+                    specs.put("intensite", 30);
+                }
+                
+
+            }
+            //System.out.println(specs);
+            objet.setDonneesJson(specs);
         }
-    }
-}
+
+        Parametre_objetRepository.saveAll(objets);
+    }}
