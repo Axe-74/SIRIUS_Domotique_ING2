16ab7b617adf9e8b34bfdf90ea3c6a980fa2e6d2
Axel <axel.jacomme@etu.u-pec.fr>
2026-04-02 22:33:27 +0200

    Lancement du mock de Température Intérieure

--- back/src/main/java/sirius/back/controllers/SimuAccelereeController.java
+++ back/src/main/java/sirius/back/controllers/SimuAccelereeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sirius.back.mock.TempInterieureSimulation;
 import sirius.back.mock.TempSimulationAcceleree;
 
 @RestController
@@ -17,11 +18,23 @@ public class SimuAccelereeController {
     @Autowired
     private TempSimulationAcceleree tempSimulationAcceleree;
 
+    @Autowired
+    private TempInterieureSimulation tempInterieureSimulation;
+
     @PostMapping("journee")
     public ResponseEntity<String> ajouterTempSimulationAcceleree() {
 
         new Thread(() -> {
             tempSimulationAcceleree.lancerSimulation();
+        }).start();
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            tempInterieureSimulation.lancerSimulation();
         }).start();
 
         return new ResponseEntity<>("Simulation d'une journée en 2 minutes lancée", HttpStatus.CREATED);
