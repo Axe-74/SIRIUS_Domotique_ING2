d834b67d3bf1c5a3964901886e0efb7f216d8eac
Axel <axel.jacomme@etu.u-pec.fr>
2026-04-23 18:42:50 +0200

    V1 de tentative de modif de l'affichage de la présence ou non d'une personne dans la pièce

--- front/src/modules/mesures/components/mesuresHistoriqueComponents.js
+++ front/src/modules/mesures/components/mesuresHistoriqueComponents.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer} from 'recharts';
+import {LineChart, ComposedChart, Line, CartesianGrid, XAxis, YAxis, Area, Tooltip, ResponsiveContainer} from 'recharts';
 
 export default function MesuresHistoriqueComponents({ data }) {
     return (
         <ResponsiveContainer width='100%' height={600}>
-            <LineChart
+            <ComposedChart
                 width={1100}
                 height={500}
                 data={data}
@@ -16,8 +16,9 @@ export default function MesuresHistoriqueComponents({ data }) {
                 <Tooltip />
                 <Line type="monotone" dataKey="valeurExt" stroke="#8884d8" dot={false} name="Température Extérieure"/>
                 <Line type="monotone" dataKey="valeurInt" stroke="#ff7300" dot={false} name="Température Intérieure"/>
-                <Line type="step" dataKey="valeurMouv" stroke="#28a745" dot={false} name="Présence" />
-            </LineChart>
+                <Area type="step" dataKey="valeurMouv" stroke="#28a745" dot={false} name="Présence" fillOpacity={1} fill="#28a745"/>
+
+            </ComposedChart>
         </ResponsiveContainer>
     );
 }
