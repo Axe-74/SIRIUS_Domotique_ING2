7b37fc13881e80c28136c156b0a9c43c6ee977fc
Enzo Dupuis <enzo.dupuis@etu.u-pec.fr>
2026-04-26 23:38:18 +0200

    Ajout de prise en compte de l'etat de l'automatisation lors de méthode permettant d'agir sur les objets lors de la simulation

--- back/src/main/java/sirius/back/repositories/Parametre_objetRepository.java
+++ back/src/main/java/sirius/back/repositories/Parametre_objetRepository.java
@@ -1,6 +1,7 @@
 package sirius.back.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import sirius.back.models.Parametre_objet;
 import sirius.back.models.Piece;
@@ -10,5 +11,11 @@ import java.util.List;
 @Repository
 public interface Parametre_objetRepository extends JpaRepository<sirius.back.models.Parametre_objet, Long> {
     List<Parametre_objet> findByPieces(Piece piece);
+
+    @Query(value = "SELECT DISTINCT p.* FROM parametre_objet p " +
+            "JOIN automatisation_parametre_objet l ON p.id_objet = l.id_objet " +
+            "JOIN automatisation a ON l.id_automatisation = a.id_automatisation " +
+            "WHERE a.etat = true", nativeQuery = true)
+    List<Parametre_objet> findObjetsAvecAutomatisationActive();
 }
 
